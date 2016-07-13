072994c750655cc0a5111990f318e6f48633f2ad
Jason Decastro <jasonrdecastro@gmail.com>
2016-07-13 00:02:17 -0400

    this is the code to power the website

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+def checkStatus():
+	time = requests.get('https://pgorelease.nianticlabs.com/plfe/').elapsed.total_seconds()
+	if time < 3:
+		return True
+	else:
+		return False
+
+@app.route('/')
+def index():
+    if checkStatus():
+    	return render_template('index.html', status=True)
+    else:
+    	return render_template('index.html', status=False)
+
+if __name__ == "__main__":
+    app.run()
+
