e1cc4d2a1d97ecd9021e92832cb6a67c41f06451
Jason Decastro <jasonrdecastro@gmail.com>
2016-07-16 12:12:22 -0400

    fixed bug

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def checkStatus():
-	time = requests.get('https://pgorelease.nianticlabs.com/plfe/').elapsed.total_seconds()
-	if time < 3:
+	try:
+		time = requests.get('https://pgorelease.nianticlabs.com/plfe/').elapsed.total_seconds()
+		if time < 3:
+			return True
+		else:
+			return False
+	except:
 		return True
-	else:
-		return False
 
 @app.route('/')
 def index():
